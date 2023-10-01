fe858c7f5a0b98870acf0edf5dbe7109de9921d6
Manish Prajapati <manishprajapat41@gmail.com>
2023-10-02 02:17:18 +0530

    d

--- src/app/page.js
+++ src/app/page.js
@@ -7,6 +7,21 @@ import React from 'react'
 export const metadata = {
 	title: 'Laravel Live Jaipur - Find your community',
 	description: 'Laravel Live Jaipur - Find your community in Jaipur',
+	keywords: ['Laravel Live Jaipur', ' Find your community in Jaipur', 'laravellivejaipur'],
+	metadataBase: new URL('https://www.laravellivejaipur.in'),
+	images: [
+		{
+			url: 'https://nextjs.org/og.png',
+			width: 800,
+			height: 600,
+		},
+		{
+			url: 'https://nextjs.org/og-alt.png',
+			width: 1800,
+			height: 1600,
+			alt: 'My custom alt',
+		},
+	],
 }
 
 export default function Home() {
